70d3f43a6b7016da77a863713fc9d3e417606d12
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-02-28 07:30:17 +0800

    v0.1.0-beta

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Glass-Cannon-GUI
+<div style="text-align: center">
+<img alt="GitHub release (latest SemVer including pre-releases)" src="https://img.shields.io/github/v/release/christianavi/Glass-Cannon-Desktop?include_prereleases&style=flat-square"> <img alt="Discord" src="https://img.shields.io/discord/660452758775922701?color=%237289DA&label=Discord&logo=discord&logoColor=white&style=flat-square">
+
+</div>
+
+<img src="./src/assets/icons/hero-01.png"/>
---
+++ src/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 440,
+    height: 558,
+    // minWidth: 440,
+    // minHeight: 558,
+    icon: "src/assets/icons/icon.ico",
+    maximizable: false,
+    resizable: false,
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.webContents.on('new-window', function (e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
+
+};
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
