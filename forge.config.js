2661db4f6be2d358985eb1599ebe5a1b5f2ccddd
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-02-28 15:26:59 +0800

    Automatic updates

--- forge.config.js
+++ forge.config.js
@@ -13,18 +13,14 @@ module.exports = {
             }
         },
         {
-            "name": "@electron-forge/maker-zip",
-            "platforms": [
-                "darwin"
-            ]
-        },
-        {
-            "name": "@electron-forge/maker-deb",
-            "config": {}
-        },
-        {
-            "name": "@electron-forge/maker-rpm",
-            "config": {}
+            "name": "electron-forge/publisher-github",
+            "config": {
+                "respository": {
+                    "owner": "christianavi",
+                    "name": "Glass-Cannon-Desktop"
+                },
+                "prerelease": true
+            }
         }
     ]
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require('update-electron-app')()
+
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 if (require('electron-squirrel-startup')) {
