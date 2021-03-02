23e5d530c1df18e69482b0f0b6c21a8c9eef988e
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-03-03 07:49:42 +0800

    app.asar

--- forge.config.js
+++ forge.config.js
@@ -2,12 +2,14 @@ const path = require('path')
 
 module.exports = {
     "packagerConfig": {
-        "icon": path.join(__dirname, "src", "assets", "icons", "icon.ico")
+        "icon": path.join(__dirname, "src", "assets", "icons", "icon.ico"),
+        "asar": true
     },
     "makers": [{
             "name": "@electron-forge/maker-squirrel",
             "config": {
                 "name": "Glass-Cannon-Desktop",
+                "copyright": "Christian Avi Bulan",
                 "iconUrl": "https://raw.githubusercontent.com/christianavi/Glass-Cannon-Desktop/main/src/assets/icons/icon.ico",
                 "setupIcon": path.join(__dirname, "src", "assets", "icons", "icon.ico")
             }
