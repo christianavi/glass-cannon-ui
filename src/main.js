50c4c19ab00034f67bc92654be0e713fd4acbd37
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-03-10 12:19:12 +0800

    Fixed multiple instances

--- src/main.js
+++ src/main.js
@@ -8,66 +8,82 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 440,
-    height: 558,
-    // minWidth: 440,
-    // minHeight: 558,
-    icon: path.join(__dirname, "assets", "icons", "icon.ico"),
-    maximizable: false,
-    resizable: false,
-    frame: false,
-    transparent: true,
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  function createWindow() {
+    const mainWindow = new BrowserWindow({
+      width: 440,
+      height: 558,
+      // minWidth: 440,
+      // minHeight: 558,
+      icon: path.join(__dirname, "assets", "icons", "icon.ico"),
+      maximizable: false,
+      resizable: false,
+      frame: false,
+      transparent: true,
+      webPreferences: {
+        nodeIntegration: true,
+        enableRemoteModule: true
+      }
+    });
+  
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    mainWindow.webContents.on('new-window', function (e, url) {
+      e.preventDefault();
+      require('electron').shell.openExternal(url);
+    });
+    console.log("Start tray")
+    let tray = null
+    app.whenReady().then(() => {
+      tray = new Tray(path.join(__dirname, "assets", "icons", "icon.ico"))
+      const contextMenu = Menu.buildFromTemplate([{
+          label: 'Glass Cannon',
+          click: async () => {
+            mainWindow.show()
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Exit',
+          click: async () => {
+            app.quit()
+          }
+        }
+      ])
+      tray.setToolTip('Glass Cannon')
+      tray.setContextMenu(contextMenu)
+      
+      tray.on('click', (event) => {
+        mainWindow.show()
+      })
+    })
+
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore()
+        mainWindow.focus()
+      }
+    })
+  
+  };
+  app.on('ready', createWindow);
+  
+  
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
     }
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.webContents.on('new-window', function (e, url) {
-    e.preventDefault();
-    require('electron').shell.openExternal(url);
-  });
-  console.log("Start tray")
-  let tray = null
-  app.whenReady().then(() => {
-    tray = new Tray(path.join(__dirname, "assets", "icons", "icon.ico"))
-    const contextMenu = Menu.buildFromTemplate([{
-        label: 'Glass Cannon',
-        click: async () => {
-          mainWindow.show()
-        }
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Exit',
-        click: async () => {
-          app.quit()
-        }
-      }
-    ])
-    tray.setToolTip('Glass Cannon')
-    tray.setContextMenu(contextMenu)
-  })
-
-  console.log("End tray")
-
-};
-app.on('ready', createWindow);
-
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+}
